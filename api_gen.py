efa107b26fb2fd98f1239467f12ad7bd0661bebb
Benjamin Eppinger <bxe5056@gmail.com>
2018-03-30 19:03:13 -0400

    Made Program Modular

---
+++ api_gen.py
@@ -0,0 +1,38 @@
+from pprint import pprint
+import json
+
+
+class api_gen:
+    tel = {'Ben': '65da52ba54ce4f99b795a3ebaead6b02', 'default': 'abcd'}
+
+    def __init__(self):  # this method creates the class object.
+        self.importKeys()
+        pass
+
+    def importKeys(self):
+        while True:
+            try:
+                self.tel = json.load(open("apiKeys.txt"))
+                break
+            except IOError:
+                print "No Data Input"
+                print "Creating API Key File"
+                self.exportKeys()
+                break
+
+    def exportKeys(self):
+        json.dump(self.tel, open("apiKeys.txt", 'w'))
+
+    def addKey(self, label, key):
+        self.tel[label] = key
+        self.exportKeys()
+
+    def getKey(self, key):
+        a = self.tel[key]
+        return a
+
+    def printAllKeys(self):
+        print "Available API Keys:"
+        for x in self.tel:
+            print "\t",str(x), "\t:", str(unicode(self.tel[x]))
+
---
+++ autorun.py
@@ -0,0 +1,35 @@
+from api_gen import api_gen
+from news_crawler import news_crawler
+from data_collection import data_collection
+import json
+
+print ""
+print "***Welcome to Edu Bias News Crawler***"
+print ""
+
+# API Key Selection
+apiKeys = api_gen()
+apiKeys.printAllKeys()
+print ""
+currentAPIKey = apiKeys.getKey(str(raw_input("Select an API Key: ")))
+
+
+# Data Collection Module
+inputPrompt = "Run Data Collection Module (y/n):"
+if(str(raw_input(inputPrompt)) == 'y'):
+    data = data_collection()
+    article_batches = int(raw_input("\tNumber of Article Batches: "))
+    data_export = data.collect_articles(article_batches, currentAPIKey)
+    json.dump(data_export, open("data_collection.txt", 'w'))
+else:
+    print "Loading Cached Datafile (data_collection.txt)"
+    data_export = json.load(open("data_collection.txt"))
+
+# Crawler Module
+inputPrompt = "Run Crawler Module (y/n):"
+if(str(raw_input(inputPrompt)) == 'y'):
+    crawler = news_crawler()
+    crawler_export = crawler.crawl_articles(data_export)
+    json.dump(crawler_export, open("crawler_export.txt", 'w'))
+else:
+    print ""
