35e6ad86faf329feb2a31fecda32e065bd7deea0
Benjamin Eppinger <bxe5056@gmail.com>
2018-04-05 01:46:08 -0400

    Added WordCloud Capability to word_tools

--- word_tools.py
+++ word_tools.py
@@ -1,4 +1,6 @@
-import re
+import re, json
+from word_tools_wordcloud import word_tools_wordcloud as wc
+import random
 
 
 class word_tools:
@@ -7,12 +9,18 @@ class word_tools:
         self.keywords = ''
         pass
 
+    def grey_color_func(word, font_size, position, orientation, random_state=None,
+                        **kwargs):
+        return "hsl(0, 0%%, %d%%)" % random.randint(60, 100)
+
     def textToWordCount(self, inputDictionary):
         # print inputDictionary
         inner_dictionary = inputDictionary["articles"]
         # print len(inner_dictionary)
         # print type(inner_dictionary)
         counter = 0
+        exitDictionary = {}
+        master_frequency = {}
         while counter < len(inner_dictionary):
             a = inner_dictionary[counter]
             b = a['article']
@@ -22,8 +30,64 @@ class word_tools:
             for word in match_pattern:
                 count = frequency.get(word, 0)
                 frequency[word] = count + 1
+                master_count = master_frequency.get(word, 0)
+                master_frequency[word] = master_count + 1
             b = b.split()
             a['wordFreq'] = frequency
+            keyTag = str(counter)
+            exitDictionary[keyTag] = frequency
             counter = counter + 1
-        # print type(a)
+
+        t = json.dumps(exitDictionary, indent=4, sort_keys=True)
+        j = t
+        f = open('single_word_count_export.json', 'w')
+        print >> f, j
+        f.close()
+
+        outDict = {}
+        try:
+            print "Attempting to import stopWords.txt"
+            stopWords = [line.rstrip('\n') for line in open('stopWords.txt')]
+        except Exception as ke:
+            print "Warning: stopWords.txt was not found - Using Default Stopwords"
+            stopWords = ["com", "not", "http", "https", "said", "news", "bbc", "www", "a", "about", "above", "after",
+                         "again", "against", "all", "am", "an", "and", "any", "are", "as", "at", "be", "because",
+                         "been", "before", "being", "below", "between", "both", "but", "by", "could", "did", "do",
+                         "does", "doing", "down", "during", "each", "few", "for", "from", "further", "had", "has",
+                         "have", "having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself",
+                         "him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in",
+                         "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my", "myself",
+                         "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours", "ourselves", "out",
+                         "over", "own", "same", "she", "she'd", "she'll", "she's", "should", "so", "some", "such",
+                         "than", "that", "that's", "the", "their", "theirs", "them", "themselves", "then", "there",
+                         "there's", "these", "they", "they'd", "they'll", "they're", "they've", "this", "those",
+                         "through", "to", "too", "under", "until", "up", "very", "was", "we", "we'd", "we'll", "we're",
+                         "we've", "were", "what", "what's", "when", "when's", "where", "where's", "which", "while",
+                         "who", "who's", "whom", "why", "why's", "with", "would", "you", "you'd", "you'll", "you're",
+                         "you've", "your", "yours", "yourself", "yourselves"]
+        minimumTermCount = int(raw_input("Minimum Term Frequency to Count in JSON (Default: 15): ") or "15")
+        for key in master_frequency:
+            if master_frequency[key] > minimumTermCount and key not in stopWords:
+                outDict[key] = master_frequency[key]
+
+        t = json.dumps(outDict, indent=4, sort_keys=True)
+        j = t
+        f = open('master_freq.json', 'w')
+        print >> f, j
+        f.close()
+
+        inputPrompt = "\nCreate Standard WordCloud? (y/n):"
+        if (str(raw_input(inputPrompt)) == 'y'):
+            wt = wc()
+            print 'Exporting WordCloud to /Visualizations/WordCloud.jpg'
+            ot = wt.make_wordcloud(outDict, 'Visualizations/WordCloud.jpg')
+
+        inputPrompt = "\nCreate WordCloud From Image? (y/n):"
+        if (str(raw_input(inputPrompt)) == 'y'):
+            inputPrompt = "\tJPG Filename (Default: stencil.jpg): "
+            filename = str(raw_input(inputPrompt) or "stencil.jpg")
+            wt = wc()
+            print 'Exporting WordCloud to /Visualizations/WordCloud_Img.jpg'
+            ot = wt.make_wordcloud_img(outDict, filename, 'Visualizations/WordCloud_Img.jpg')
+
         return inputDictionary
---
+++ word_tools_wordcloud.py
@@ -0,0 +1,62 @@
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+import numpy as np
+from PIL import Image
+from os import path
+import matplotlib.pyplot as plt
+import random
+
+
+class word_tools_wordcloud:
+    def __init__(self):
+        self.d = path.dirname(__file__)
+        pass
+
+    def make_wordcloud(self, in_dictionary, out_file):
+        the_cloud = WordCloud().generate_from_frequencies(in_dictionary)
+
+        import matplotlib.pyplot as plt
+        plt.imshow(the_cloud, interpolation='bilinear')
+        plt.axis("off")
+
+        the_cloud = WordCloud(relative_scaling=1, width=2048, height=2048, max_words=2048, min_font_size=1,
+                              mode='RGB', normalize_plurals=True).generate_from_frequencies(in_dictionary)
+        plt.figure()
+        plt.imshow(the_cloud, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
+
+        the_cloud.to_file(out_file)
+
+        return the_cloud
+
+    def make_wordcloud_img(self, in_dictionary, in_image, out_file):
+        # read the mask_color image
+        the_mask = Image.open(path.join(self.d, in_image))
+        mask_color = np.array(the_mask)
+
+        # create WordCloud Process
+        the_cloud = WordCloud().generate_from_frequencies(in_dictionary)
+
+        import matplotlib.pyplot as plt
+        plt.imshow(the_cloud, interpolation='bilinear')
+        plt.axis("off")
+
+        the_cloud = WordCloud(background_color='white', mask=mask_color, relative_scaling=1, margin=8, width=2048,
+                              height=2048, max_words=999, min_font_size=6, mode='RGB',
+                              normalize_plurals=True, color_func=ImageColorGenerator(mask_color)
+                              ).generate_from_frequencies(in_dictionary)
+
+        default_colors = the_cloud.to_array()
+        plt.title("Custom colors")
+        plt.imshow(the_cloud.recolor(random_state=3),
+                   interpolation="bilinear")
+
+        the_cloud.to_file(out_file)
+
+        plt.axis("off")
+        plt.figure()
+        plt.imshow(default_colors, interpolation="bilinear")
+        plt.axis("off")
+        plt.show()
+
+        return the_cloud
